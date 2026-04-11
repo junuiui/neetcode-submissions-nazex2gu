87ae7c5fd6e8e9dca6c5cd8e1d07c5a2bd08c64f
Jun (Junui) Hong <80214846+junuiui@users.noreply.github.com>
2026-04-10 18:16:08 -0700

    Add: linked-list-cycle-detection - submission-6

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-6.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        """
+        (X) if values are distincts, we store it in hash set
+
+        """
+        # visited = set()
+
+        # while head:
+        #     if head.val in visited:
+        #         return True
+        #     else:
+        #         visited.add(head.val)
+        #     head = head.next
+        
+        # return False
+
+        visited = set()
+
+        while head:
+            if head in visited:
+                return True
+            else:
+                visited.add(head)
+            head = head.next
+        
+        return False
+
+
