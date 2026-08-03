cd8c2a4961a69a603f1250afbdbff97654d71205
Jun (Junui) Hong <80214846+junuiui@users.noreply.github.com>
2026-08-03 16:06:00 -0700

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.py
@@ -0,0 +1,14 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        
+        seen = set()
+
+        for val in nums:
+            if val in seen:
+                return True
+            
+            seen.add(val)
+    
+        return False
+        
+        
